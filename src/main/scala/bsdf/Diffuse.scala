3e4c027dfbbaa78f2798f8b3f8e34e55acd50805
Gon Solo <gonsolo@gmail.com>
2023-11-22 11:26:29 +0100

    Clean up and add a failing test.

--- src/main/scala/bsdf/Diffuse.scala
+++ src/main/scala/bsdf/Diffuse.scala
@@ -8,161 +8,34 @@ import hardfloat._
 object CONSTANTS {
   def EXPONENT_BITS = 8
   def SIGNIFICAND_BITS = 23
-  def WIDTH = (EXPONENT_BITS + SIGNIFICAND_BITS).W
-
-  val TEST_VAL = 666.U(CONSTANTS.WIDTH)
-//  val NUMBER_SPECTRUM_SAMPLES = 4
-//  //val INVERSE_PI: Float = 1.0.toFloat / Pi.toFloat
+  def EXP_SIG_WIDTH = (EXPONENT_BITS + SIGNIFICAND_BITS).W
+  def WIDTH = (EXPONENT_BITS + SIGNIFICAND_BITS + 1).W
 }
 
-
-// Single precision floating point
-//class PepeFloat extends Bundle {
-//
-//  def signBits = 1
-//  def significandBits = 23
-//  def exponentBits = 8
-//  def bits = signBits + exponentBits + significandBits
-//
-//  val sign = Bool()
-//  val significand = UInt(significandBits.W)
-//  val exponent = UInt(exponentBits.W)
-//
-//  // TEST
-//  //val i2f = Module(new hardfloat.INToRecFN(bits, exponentBits, significandBits))
-//
-//  // TEST
-//  def recode(x: UInt) = hardfloat.recFNFromFN(exponentBits, significandBits, x)
-//}
-
-
-//class Vector3 extends Bundle {
-//  val x = new PepeFloat
-//  val y = new PepeFloat
-//  val z = new PepeFloat
-//}
-
-//class SampledSpectrum extends Bundle {
-//  val values = Vec(CONSTANTS.NUMBER_SPECTRUM_SAMPLES, new PepeFloat)
-//}
-
-//class BsdfInputBundle extends Bundle {
-//  val outDirection = new Vector3
-//  val inDirection = new Vector3
-//}
-
-//class BsdfOutputBundle extends Bundle {
-//  val ratio = new SampledSpectrum
-//}
-
-//class MultiplyPipe() extends Module {
-//  val io = IO(new Bundle {
-//    val a = Input(Bits((8 + 23).W))
-//    val b = Input(Bits((8 + 23).W))
-//    val out = Output(Bits((8 + 23 + 1).W))
-//  })
-//  val mul = Module(new hardfloat.MulRecFN(8, 23))
-//  mul.io.a := hardfloat.recFNFromFN(8, 23, io.a)
-//  mul.io.b := hardfloat.recFNFromFN(8, 23, io.b)
-//  mul.io.roundingMode := 0.U // nearEven
-//  mul.io.detectTininess := 0.U // tininess before
-//  mul.io.out := mul.io.out
-//  val exceptionFlags = mul.io.exceptionFlags
-//}
-
-//class MultiplySpectrum extends Module {
-//  val io = new Bundle {
-//    val a = new SampledSpectrum
-//    val b = new SampledSpectrum
-//    val out = new SampledSpectrum
-//  }
-////  io.a.values.lazyZip(io.b.values).lazyZip(io.out.values).toList foreach { case (a, b, out) =>
-////    val mul = new MultiplyPipe
-////    mul.io.a := a
-////    mul.io.b := b
-////    out := mul.io.out
-////  }
-//}
-
-//object InversePi {
-//  def create() : PepeFloat = {
-//    val inversePi = Wire(new PepeFloat)
-//    inversePi.sign := false.B
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 0.U
-//    //inversePi.significand(0) := 1.U
-//    //inversePi.exponent(0) := 1.U
-//    //inversePi.exponent(0) := 0.U
-//    //inversePi.exponent(0) := 0.U
-//    //inversePi.exponent(0) := 0.U
-//    //inversePi.exponent(0) := 0.U
-//    //inversePi.exponent(0) := 0.U
-//    //inversePi.exponent(0) := 1.U
-//    //inversePi.exponent(0) := 1.U
-//    // 32 bit inv pi: sign sig exp 0 01111101010001011111001 10000011
-//    inversePi
-//  }
-//}
-
-//trait Bsdf {
-//  val directions = IO(Flipped(Decoupled(new BsdfInputBundle)))
-//  //val output = IO(Decoupled(new BsdfOutputBundle))
-//}
-
-//class Diffuse extends Module with Bsdf {
 class Diffuse extends Module {
-//  val reflectance = IO(Flipped(Decoupled(new SampledSpectrum)))
-//
-//  reflectance.ready := true.B
-//  directions.ready := true.B
-//
-//  //val inversePi = IO(InversePi.create())
-//
-//  //val inversePi = new PepeFloat
-//  //inversePi.sign := false.B
-//  //inversePi.sign := false.B
-//  //inversePi.significand(0) := 0.U
-//  val inversePiSpectrum = Reg(new SampledSpectrum)
-//
-//  val multiply = Module(new MultiplySpectrum())
-//  //multiply.io.a := reflectance.bits
-//  //multiply.io.b := inversePiSpectrum
-//  //output.bits.ratio := multiply.io.out
-//  // Also testing fails now!
-//
-//  //output.valid := true.B
-//  //output.bits.ratio := IO(new SampledSpectrum)
 
-  val mul = Module(new MultiplyPipe())
-  mul.io.a := CONSTANTS.TEST_VAL
-  mul.io.b := CONSTANTS.TEST_VAL
-}
-
-class MultiplyPipe extends Module {
   val io = IO(
     new Bundle {
       val a = Input(Bits(CONSTANTS.WIDTH))
       val b = Input(Bits(CONSTANTS.WIDTH))
+      val out = Output(Bits(CONSTANTS.WIDTH))
+    }
+  )
+
+  val mul = Module(new Multiply())
+  mul.io.a := io.a
+  mul.io.b := io.b
+  io.out := mul.io.out
+}
+
+class Multiply extends Module {
+
+  val io = IO(
+    new Bundle {
+      val a = Input(Bits(CONSTANTS.WIDTH))
+      val b = Input(Bits(CONSTANTS.WIDTH))
+      val out = Output(Bits(CONSTANTS.WIDTH))
+      val exceptionFlags = Output(Bits(5.W))
     }
   )
 
@@ -171,8 +44,11 @@ class MultiplyPipe extends Module {
   mul.io.b := recode(io.b)
   mul.io.roundingMode := 0.U
   mul.io.detectTininess := 0.U
+  io.out := decode(mul.io.out)
+  io.exceptionFlags := mul.io.exceptionFlags
 
   def recode(x: UInt) = hardfloat.recFNFromFN(CONSTANTS.EXPONENT_BITS, CONSTANTS.SIGNIFICAND_BITS, x)
+  def decode(x: UInt) = hardfloat.fNFromRecFN(CONSTANTS.EXPONENT_BITS, CONSTANTS.SIGNIFICAND_BITS, x)
 }
 
 import circt.stage.ChiselStage
--- src/test/scala/bsdf/DiffuseSpec.scala
+++ src/test/scala/bsdf/DiffuseSpec.scala
@@ -6,9 +6,16 @@ import org.scalatest.freespec.AnyFreeSpec
 
 class DiffuseSpec extends AnyFreeSpec with ChiselScalatestTester {
 
-  "Diffuse should return 1" in {
-    test(new Diffuse) { dut =>
-      //dut.output.bits.ratio.r.foo.expect(1.U)
+  val a = 1116025651.U // float32: 66.6
+  val b = 1073741824.U // float32: 2.0
+  val c = 1124414259.U // float32: 133.2
+
+  "Diffuse should multiply correctly" in {
+    test(new Diffuse) { diffuse =>
+      diffuse.io.a.poke(a)
+      diffuse.io.b.poke(b)
+      diffuse.clock.step(100)
+      diffuse.io.out.expect(c)
     }
   }
 }
