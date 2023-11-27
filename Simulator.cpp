bbb9409c5d72f627f25374ca7272be451f6a15ee
Gon Solo <gonsolo@gmail.com>
2023-11-27 12:16:10 +0100

    Restructure simulator.

--- Simulator.cpp
+++ Simulator.cpp
@@ -23,20 +23,12 @@ float uint32ToFloat(uint64_t value) {
         return bit_cast<float>((uint32_t)value);
 }
 
-int main(int argc, char** argv) {
-        Verilated::commandArgs(argc, argv);
-	VDiffuse diffuse;
-        diffuse.reset = 0;
-        auto rootp = diffuse.rootp;
-
-	float a = 33.2;
-	float b = 2.7;
+void test_multiply(VDiffuse& diffuse, float a, float b) {
 	float expected = a * b;
-        rootp->io_a = floatToUint32(a);
-        rootp->io_b = floatToUint32(b);
-
+        diffuse.rootp->io_a = floatToUint32(a);
+        diffuse.rootp->io_b = floatToUint32(b);
 	auto steps = 4;
-        for (uint64_t main_time  = 0; main_time < steps; main_time++) {
+        for (uint64_t main_time = 0; main_time < steps; main_time++) {
                 if (main_time == 0) {
                         diffuse.reset = 1;
                 } else {
@@ -45,13 +37,18 @@ int main(int argc, char** argv) {
                 diffuse.clock = main_time;
                 diffuse.eval();
         }
-
-        float out = uint32ToFloat(rootp->io_out);
+        float out = uint32ToFloat(diffuse.rootp->io_out);
 	//cout << "a: " << a << ", b: " << b << ", expected: " << expected << ", out: " << out << endl;
 	check(out == expected, "out == a * b");
+}
 
+int main(int argc, char** argv) {
+        Verilated::commandArgs(argc, argv);
+	VDiffuse diffuse;
+        diffuse.reset = 0;
+        auto rootp = diffuse.rootp;
+	test_multiply(diffuse, 33.2, 2.7);
         diffuse.final();
 	cout << "Ok." << endl;
 }
-
 
