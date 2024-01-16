384b4cc35b088636f994a4716948a4158d0f1cd7
Gon Solo <gonsolo@gmail.com>
2024-01-16 16:41:58 +0100

    More tests.

--- src/test/scala/bsdf/DiffuseSpec.scala
+++ src/test/scala/bsdf/DiffuseSpec.scala
@@ -4,16 +4,13 @@ import chisel3._
 import chisel3.experimental.BundleLiterals._
 import chiseltest._
 import java.lang.Float.floatToIntBits
-//import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.flatspec.AnyFlatSpec
 import scala.math.Pi
 
-//class DiffuseSpec extends AnyFreeSpec with ChiselScalatestTester {
 class DiffuseSpec extends AnyFlatSpec with ChiselScalatestTester {
 
   behavior of "Diffuse"
   
-  //"Diffuse should return inv pi" in {
   it should "return inv pi" in {
     test(new Diffuse).withAnnotations(Seq(WriteVcdAnnotation)) { diffuse =>
       diffuse.input.initSource()
@@ -21,7 +18,7 @@ class DiffuseSpec extends AnyFlatSpec with ChiselScalatestTester {
       diffuse.output.initSink()
       diffuse.output.setSinkClock(diffuse.clock)
 
-      val testValues = for { x <- 1 to 1 } yield x.toFloat / 3.0f
+      val testValues = for { x <- -4 to 4 } yield x.toFloat / 3.0f
       val inputSeq = testValues.map { case x =>
         (new DiffuseInputBundle).Lit(
             _.reflectance.values(0) -> floatToIntBits(x).S //,
@@ -37,19 +34,14 @@ class DiffuseSpec extends AnyFlatSpec with ChiselScalatestTester {
         )
       }
 
-      //fork {
-      //  diffuse.input.enqueueSeq(inputSeq)
-      //}.fork {
-      //  diffuse.output.expectDequeueSeq(resultSeq)
-      //}.join()
-
-      fork {
-        diffuse.input.enqueueSeq(inputSeq)
+      for ((a, b) <- inputSeq zip resultSeq) {
+        diffuse.output.expectInvalid()
+        diffuse.input.enqueueNow(a)
+        diffuse.output.expectInvalid()
+        diffuse.clock.step(5)
+        diffuse.output.expectDequeueNow(b)
+        diffuse.clock.step(1)
       }
-      diffuse.output.expectInvalid()
-      diffuse.clock.step(5)
-      //diffuse.output.expectDequeueNow((new DiffuseOutputBundle).Lit(_.out.values(0) -> floatToIntBits(1.0f / 3.0f / Pi.toFloat).S))
-      diffuse.output.expectDequeueNow(resultSeq(0))
     }
   }
 }
